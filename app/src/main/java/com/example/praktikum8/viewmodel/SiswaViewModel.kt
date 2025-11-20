f3798f73603215231a83ce9840cd04d68f172b8a
kiieeaa <kiyaadinda5@gmail.com>
2025-11-20 17:59:35 +0700

    Fungsi untuk memperbarui state Siswa berdasarkan input user

--- app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
@@ -11,3 +11,9 @@ import kotlinx.coroutines.flow.update
 class SiswaViewModel : ViewModel(){
     private val _statusUI = MutableStateFlow(value = Siswa())
     val  statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+    fun setSiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatini ->
+            statusSaatini.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
+        }
+    }
+}

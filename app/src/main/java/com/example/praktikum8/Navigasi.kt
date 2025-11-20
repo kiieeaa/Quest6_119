9dff3261115faf105bae545a066633081d30f1a0
kiieeaa <kiyaadinda5@gmail.com>
2025-11-20 17:49:50 +0700

    Import utama untuk navigasi, UI, dan ViewModel pada aplikasi siswa

---
+++ app/src/main/java/com/example/praktikum8/Navigasi.kt
@@ -0,0 +1,26 @@
+package com.example.praktikum8
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum8.model.DataJK.JenisK
+import com.example.praktikum8.view.FormSiswa
+import com.example.praktikum8.view.TampilSiswa
+import com.example.praktikum8.viewmodel.SiswaViewModel
+
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable

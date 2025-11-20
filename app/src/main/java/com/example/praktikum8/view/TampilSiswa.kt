c7b9ede9b91c9037859c22609bf047e6bf3dac76
kiieeaa <kiyaadinda5@gmail.com>
2025-11-20 17:39:19 +0700

    Implementasi fungsi TampilSiswa dengan parameter Siswa dan callback back button

--- app/src/main/java/com/example/praktikum8/view/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/TampilSiswa.kt
@@ -31,3 +31,8 @@ import com.example.praktikum8.model.Siswa
 
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked:()->Unit
+){

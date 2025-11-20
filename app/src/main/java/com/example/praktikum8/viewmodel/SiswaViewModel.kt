b98aa346fcd0ed4752e266a1b197cde5943ecc47
kiieeaa <kiyaadinda5@gmail.com>
2025-11-20 17:58:20 +0700

    Import ViewModel dan Flow untuk mengelola state siswa secara realtime

---
+++ app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum8.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikum8.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
